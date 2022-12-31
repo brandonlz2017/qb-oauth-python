26e980d5b5797cad7f0a39f6dac8eb7c5a910221
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2022-12-31 15:52:31 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,20 +1,20 @@
 client_secrets = {
-    "client_id": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-    "client_secret": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "client_id": "ABrWneIZutckh2FSFDa1Qsp2Ud6JZGKjEkA9rOa144QvdrfmgW",
+    "client_secret": "HNFvyg8OFcQkHELNfAGARzWgXKAge01OnWAtdCsc",
     "redirect_uri": "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl",
     "environment": "sandbox"
 }
 
 qBooksAssets = {
     "authentication_assets" : {
-        "realm_id": 000000000000000000000000000000000,
-        "auth_code": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        "realm_id": 4620816365266151130,
+        "auth_code": "AB11672520049Tdwd3riUPuviUcoBYhKnTUovayC4bzrlFwPWF"
     }
 }
 
-refreshToken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+refreshToken = "AB11681246196zUcpgcJkG6GGTA4DsBQKvuwbDUXUXrdrDEGjh"
 
 qBData = {
-    "realm_id":"XXXXXXXXXXXXXXXXXXXXXX",
-    "accessToken":"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    "realm_id":"4620816365266151130",
+    "accessToken":"eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..b0A2XFObWZ2x2GMn54H-Cw.7NMIq49d8FhwsAHx1Q7ei1sZ1jh4-_4tVAFJhdDKRYihbk2s279aDfVOJj-Sbym_oiIPBAP8VPjQ1GmhfIeSgflPBWId_r1OX8pkPsDwL3i0MU13-I5CD-b1AiRPy0rmP4MXlXW9FtXnO2NvLSV3JIN3n1khoCo7U5HMlx9GEOU0cyNUIKpbEsi4Kqqm2POt9lyed4jXDvvL2Se6V1p-HIIAhGPwknrDf8NCeS0-wjllG1xGR4vsQubqtb2JZoTeNqUxMHzPifjsuiE3yIC-1BKnsEDjZBV92WNQ3HMl-y-HQ3BEGEhI2sJdmVt-0353v15gC96wgHepXsxRV8YJETdGxOu5PhPFSIthzHZIaOAVo3EI2-3-eNdVAUD958w6pAvUEYerAuxr7OihxdmSpTaB2g9mHLsGBPBjLAmTh8CzMjCz0MrVBXpnnRKVl-56cV6kcMBnR6m9gNdT9QkfJe6W5ZGj_fqO1Hn1kvvlcCRtEQTG_rP3-MPL0wpyt3hUCGR6JgPNdmnDS9KSMWJK1CQPPg3oHVrVFqLSgey45pJyl5YE5Fgf_9k6vAoMonvSEkmqSxCq5yEj7VSZHNHtjbtbpq2TjIwAwcjXJeEzTyiguWokHPMsTpyYvBdNbCeaEYN27Iwj2rXajqex_Z8Dg5la-O4mF5J-1flCFw5vrRTBAHw8GzL8al7YrG5ev5kyyVCTfHvlz9O5KgUao0wdhiJmDhyKbPuySGq32u1fUBXJL4lTewzdPvGVmrNPKbWom_RQo_mXq8RpdMFiLCc9skMeAu_fbSr6BAwfkRL7uN8vdKI_o6F0Umwc9b1oEnJsnJ5raINj6vquq2eb_yeqJFTic17fPjyWWzBfs4galOJoKfKgXMySdPfeUB7RvGkQ.z_IKC1b1eBJSbJQeEqfBBA"
 }
