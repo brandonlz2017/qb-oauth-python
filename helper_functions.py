df868b4c11d0c1480810a845972f52392efce20c
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2023-01-12 00:12:47 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,2 +1,15 @@
 import pandas as pd
 
+def tableBuilder(data):
+    array = []
+    columns = ['Date', 'Account', 'Transaction Name', 'Category', 'Amount']
+    for trans in range(len(data)):
+        date_working = data[trans]['ColData'][0]['value']
+        account = data[trans]['ColData'][6]['value']
+        trans_name = data[trans]['ColData'][5]['value']
+        category = data[trans]['ColData'][7]['value']
+        amount = data[trans]['ColData'][8]['value']
+        array.append([date_working, account, trans_name, category, amount])
+    df = pd.DataFrame(array, columns=columns)
+    return df
+  
