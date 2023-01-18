6caa49bf34727b22b806cf59154dd174b444aead
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2023-01-17 22:15:18 -0500

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,20 +1,27 @@
+client_id = ""
+client_secret = ""
+realm_id = ""
+auth_code = ""
+refreshToken = ""
+accessToken = ""
+
 client_secrets = {
-    "client_id": "ABgzdeF0Ft57QHDi80oeBSF6MLVgUofcBk8yVqSbAEEHejVyZi",
-    "client_secret": "F3Ev1ctQ1FTOe6MhZGl9FRMdk8ZdzeCN3YnUiFEa",
+    "client_id": client_id,
+    "client_secret": client_secret,
     "redirect_uri": "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl",
     "environment": "production"
 }
 
 qBooksAssets = {
     "authentication_assets" : {
-        "realm_id": 9130355669228116,
-        "auth_code": "AB11673584196y1Z7HtMgZxqtIa5dWcLxM3nNDyRZHGvrBAyvD"
+        "realm_id": int(realm_id),
+        "auth_code": auth_code
     }
 }
 
-refreshToken = "AB11682310314Fh9W5z0gVNGJLVKedhBHvrxAlPJAR8TH4mjOC"
+refreshToken = refreshToken
 
 qBData = {
-    "realm_id":"9130355669228116",
-    "accessToken":"eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..BRd-uLYTbDcTmVpq-LE9RQ.6NDiWtaM-M--2PREHIyBes5vB9SZU2w2LuWZQttkSWCKW42IjIiL9aS12F_OZ8ufgCx-JbLxYHI9uOoKaCHbL5xgnhrhjMt7mKzhdjBIz79Yp9H46zSUs_BvdCMypeVPid00SRq-U6HTamyLX9rGUscWs9uM-Dfx6vx1aVqB3cACeTqIp_42ELgLYWjoaVJPgpDS45qAaRUht3X-kit6CHUcgLFc0FXPCxbyt3I4QRRy4240boZDF0Y1Loy4SwG4nhGuKTgPAhsx-XSFn0BTeqLzKQl1YjkoeMrtvgtWMQYtjo7bmR1PWBMLc8sa9nOfQOlUXYOSKSbvGyDkaEoFutxWKiueImXu1xG2Z3mjriKxBtkx_xQhJabuosxf-6KLE3Im-Bi7hxKUyT67dz64WRjyEZSNVaI-_WEsvU_KtdzNF7e5dCLxW3O6nqJZ41_SvW5ac0aC-_AQsyhMOx2SRjm8CTwJSpmibjJLq-1mPUnwhH95lBdeMrnoEgtHuQRH7Dw4J5dJs2UAVe0uE-9x71-HT2AnTyUMx4Nv7I0yZlOf_r4CBjitr2FxsnB8vbBeRmaaQDoFm7BG_wK6ZIEboinnY2QEJjdeJ5vufpRTg6u9BoaCrlJlIVJ0dfEECutsosdyozIvPluYK3u4yxwGd6ag1otRotakGpX1IhnJyZNImwzKyp4ndCYSAoWMlx8n252LbH2GmI-XC25GOso6uaXtCpPgLmJRnoIH5tQGqzz0vEfnfS2jdYtjVkuJf67PxNdgfX2QhvhKLDRrADENfKVIb56M-CTAS9PZfkRgayqBaua6WvLC5ym2sqny1j_PnvSs7hnBBLg6cTGdbPdZQY6cweFA6h2aSo0BvoEQK9mOQ2chgQGcNrdFjvnuptDr.oUpDkBUFfgIElx-cQW3WjA"
+    "realm_id":realm_id,
+    "accessToken":accessToken
 }
