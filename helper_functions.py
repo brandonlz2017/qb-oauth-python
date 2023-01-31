c0d7a95985f0c3d9f01796c412dd57d6a16dca1c
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2023-01-30 23:05:40 -0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -14,7 +14,14 @@ def tableBuilder(data):
         array.append([date_working, account, trans_name, category, amount])
     df = pd.DataFrame(array, columns=columns)
     return df
-  
+
+def transCat(value):
+    transTable = pd.read_csv('trans_desc_key.csv')
+    transDict = dict(zip(transTable['Key'].values, transTable['Value'].values))
+    for key in transDict:
+        if key in value:
+            return transDict[key]
+
 def send_email_attach(recipient, sender, app_key, attachment_name):
     yag = yagmail.SMTP(user=sender, password=app_key)
     yag.send(to=recipient, subject="Test with attachment", contents="Please see attached.", attachments=attachment_name)
