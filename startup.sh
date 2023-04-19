83813e5306ed7a7b4002a640910014205186a806
brandonlz2017 <94196603+brandonlz2017@users.noreply.github.com>
2023-04-19 17:54:03 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,2 +1,13 @@
-env:
-    client_id: ${{ production.CLIENT_ID }}
+
+
+sudo apt update
+sudo apt -y install python3-pip
+sudo apt -y install python3-venv
+
+cd qb-oauth-python
+python3 -m venv quickbooksVenv
+source quickbooksVenv/bin/activate
+pip install -r requirements.txt
+
+python3 main.py
+
